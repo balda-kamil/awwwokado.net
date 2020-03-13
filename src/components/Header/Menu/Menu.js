6da1c78f7384f59d25b0dab92b9cd6f3bb95cda1
Kamil Balda <baldakamil@gmail.com>
2020-03-13 10:06:10 +0100

    menu animation done

--- src/components/Header/Menu/Menu.js
+++ src/components/Header/Menu/Menu.js
@@ -1,33 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Menu.css';
 import MenuContent from './MenuContent/MenuContent'
 
-class Menu extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      isActive: true
-    }
-  }
+import { useSpring, animated }  from 'react-spring'
 
-  handleClick = () => {
-    this.setState({
-      isActive: !this.state.isActive
-    })
-  }
+function Menu(){
+  const [isActive, setActive] = useState(true)
 
-  render(){
-    return(
-      <>
-      <div className="menu__hamburger_container" onClick={this.handleClick}>
-        <div className={ "stick stick-1 " + (this.state.isActive ? 'open_menu close_menu' : 'open_menu')}></div>
-        <div className={ "stick stick-2 " + (this.state.isActive ? 'open_menu close_menu' : 'open_menu')}></div>
-        <div className={ "stick stick-3 " + (this.state.isActive ? 'open_menu close_menu' : 'open_menu')}></div>
-      </div>
+  const fade = useSpring({
+    opacity: isActive ? 0 : 1,
+  })
+
+  return(
+    <>
+    <div className="menu__hamburger_container" onClick={() => setActive(!isActive)}>
+      <div className={ "stick stick-1 " + (isActive ? 'open_menu close_menu' : 'open_menu')}></div>
+      <div className={ "stick stick-2 " + (isActive ? 'open_menu close_menu' : 'open_menu')}></div>
+      <div className={ "stick stick-3 " + (isActive ? 'open_menu close_menu' : 'open_menu')}></div>
+    </div>
+      <animated.div style={fade} className="menu__expanded">
         <MenuContent />
-      </>
-    )
-  }
+      </animated.div>
+    </>
+  )
 }
 
 export default Menu;
--- src/components/Header/Menu/MenuContent/MenuContent.js
+++ src/components/Header/Menu/MenuContent/MenuContent.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-
-import particle_params from './particle_params'
-
+import particle_params from './particle_params';
 class MenuContent extends Component{
   render(){
     return (
-      <div className="menu__expanded" ref="menu">
+      <>
         <Particles 
           params={particle_params}
           className="position-absolute w-100 h-100"/>
@@ -17,7 +15,7 @@ class MenuContent extends Component{
           <li>OPINIE KLIENTÓW</li>
           <li>KONTAKT</li>
         </ul>
-      </div>
+      </>
     );
   }
 }
