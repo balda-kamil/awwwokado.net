bdafc31bc6ee01c784f7974aa5d44b4c93bd7837
Kamil Balda <baldakamil@gmail.com>
2020-03-23 11:34:36 +0100

    simple form with validation

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React from 'react'
+import { withFormik, Form, Field, yupToFormErrors, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+import './ContactForm.css'
 
-function ContactForm(){
+function ContactForm({
+  errors,
+  touched,
+  isSubmitting
+}){
   return(
     <div className="form__main-wrapper">
-     <form>
-       kjndjkasd
-     </form>
+     <Form>
+       <div>
+        <Field type="email" name="email" placeholder="E-mail" />
+        { touched.email && errors.email && <p>{errors.email}</p>}
+       </div>
+       <div>
+        <Field type="name" name="name" placeholder="Imię" />
+        { touched.name && errors.name && <p>{ errors.name }</p>}
+       </div>
+       <div>
+        <Field component="textarea" name="textarea" placeholder="Treść wiadomości" />
+        { touched.textarea && errors.textarea && <p>{ errors.textarea }</p>}
+       </div>
+       <button disabled={ isSubmitting } type="submit">Wyślij</button>
+       { isSubmitting && <div className="lds-dual-ring"></div> }
+      </Form>
     </div>
   )
 }
 
-export default ContactForm;
+const formikContactForm = withFormik({
+  mapPropsToValues({ email, name, textarea }){
+    return {
+      email: email || '',
+      name: name || '',
+      textarea: textarea || '',
+    }
+  },
+  validationSchema: Yup.object().shape({
+    email: Yup.string().email('Podaj prawidłowy adres e-mail').required('Twoj email jest wymagany'),
+    name: Yup.string().min(3, 'Wpisz minimum 3 znaki').required('Twoje imie też się przyda :)'),
+    textarea: Yup.string().required('Im lepiej opiszesz nam swoje potrzeby, tym lepiej będziemy się w stanie dopasować'),
+  }),
+  handleSubmit(values, { resetForm, setSubmitting }){
+    setTimeout(() => {
+      resetForm()
+      console.log(values)
+      setSubmitting(false)
+    }, 1000)
+  }
+})(ContactForm)
+
+export default formikContactForm
