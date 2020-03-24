ccf196f944bab852c7304fa3398851904545f9d0
Kamil Balda <baldakamil@gmail.com>
2020-03-24 09:25:22 +0100

    ga code added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css'
 import Main from './components/Main/Main'
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-107689003-3');
+
 import {
   BrowserRouter as Router,
   Switch,
